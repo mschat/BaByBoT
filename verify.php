ecb607312efff7293712f3f9ad2a6029e689baa0
mschat <siwalnw@gmail.com>
2017-07-15 18:41:17 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,11 +1,7 @@
 <?php
-$access_token = 'KuCXzRz4QBneDsSyVFbSPocVsbufOVxWuhs+vg9XlhqkfXfe3JolM41YUAHV6FSo4DbX
+$access_token = 'KuCXzRz4QBneDsSyVFbSPocVsbufOVxWuhs+vg9XlhqkfXfe3JolM41YUAHV6FSo4DbX+gi4nnn9zb17Z8uV9l5lfPT8H2pt2CklucGNTq3jC3FeGYcBgOBmggeMxMh4EWrfepnTwvwfVzoHKK4LUgdB04t89/1O/w1cDnyilFU=';
 
-+gi4nnn9zb17Z8uV9l5lfPT8H2pt2CklucGNTq3jC3FeGYcBgOBmggeMxMh4EWrfepnTwvwfVzoHKK4LUgdB04t89/1O/w1c
-
-DnyilFU=';
-
-$url = 'https://powerful-sea-58445.herokuapp.com/verify.php';
+$url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
